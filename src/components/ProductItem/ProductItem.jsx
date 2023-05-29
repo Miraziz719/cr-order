5e94c8ddf63263d78f1b9adc29f11ca8e977a762
miraziz <miraziz719@gmail.com>
2023-05-30 02:30:45 +0500

    ssd

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -21,7 +21,7 @@ const ProductItem = ({product, className, onAdd, onRemove}) => {
             <div className=''>{product.title}</div>
             <div className='border border-button border-solid rounded-md overflow-hidden h-[36px] shadow-sm'>
                 <button onClick={onRemoveHandler} className='px-[16px] h-full bg-button text-button-text'>-</button>
-                <input onChange={handleChange} value={amount} className='max-w-[60px] px-[8px] h-full' type='number' inputMode='numeric' ></input>
+                <input onChange={handleChange} value={amount} className='max-w-[60px] px-[8px] h-full bg-bg' type='number' inputMode='numeric' ></input>
                 <button onClick={onAddHandler} className='px-[16px] h-full bg-button text-button-text'>+</button>
             </div>
         </div>
