fdd2c6ff071a29eeac2a10c453b5f349c100c0f3
miraziz <miraziz719@gmail.com>
2023-05-30 21:23:10 +0500

    sd

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -36,20 +36,20 @@ const ProductItem = ({product, className, onChange}) => {
         setamount(event.target.value)
     }
 
-    function handleStart (e, increment) {
+    function handleStart (e, increment, touched) {
         e.preventDefault()
         
         let tid = null
         let speed = 300
 
-        function handleChange () {
-            increment ? onAddHandler() : onRemoveHandler()
+        function handleChange (first) {
+            if(!first) increment ? onAddHandler() : onRemoveHandler()
             tid = setTimeout(() => {
                 handleChange()
                 speed = speed <= 100 ? 100 : speed -= 100
             }, speed)
         }
-        handleChange()
+        handleChange(touched)
 
         function handleStop () {
             clearTimeout(tid)
@@ -68,12 +68,14 @@ const ProductItem = ({product, className, onChange}) => {
             <div className='border border-button border-solid rounded-md overflow-hidden h-[36px] shadow-sm'>
                 <button 
                     className='px-[16px] h-full bg-button text-button-text'
-                    onMouseDown={e => handleStart(e)}
+                    onMouseDown={e => handleStart(e, false)}
+                    onTouchStart={e => handleStart(e, false, true)}
                 >-</button>
                 <input onChange={handleChangeInput} value={amount} className='max-w-[60px] px-[8px] h-full bg-bg' type='number' inputMode='numeric' ></input>
                 <button 
                     className='px-[16px] h-full bg-button text-button-text'
                     onMouseDown={e => handleStart(e, true)}
+                    onTouchStart={e => handleStart(e, true, true)}
                 >+</button>
             </div>
         </div>
